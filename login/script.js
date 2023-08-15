3661e3714fe67c4036f856ae1b1db0bbe7383164
mi <mirellanaspolini35@gmail.com>
2023-08-15 12:22:29 -0300

    fazendo algumas correcoes

--- login/script.js
+++ login/script.js
@@ -1,53 +1,60 @@
-var user_cadastrado = "user10";
-var senha_cadastrada = "senhaforte55";
-var imput_user = document.querySelectorAll(".login-input")[0];
-var imput_senha = document.querySelectorAll(".login-input")[1];
-var login_botao = document.querySelectorAll(".login-button")[0];
+const usuarioCadastrado = "user10";
+const senhaCadastrada = "senhaforte55";
+const txtUsuario = document.querySelector("#txtUsuario");
+const txtSenha = document.querySelector("#txtSenha");
+const btnLogin = document.querySelector(".login-button");
+const form = document.querySelector("#login-form");
 
 function spin() {
-    document.querySelector(".login-form").innerHTML += `<div class="spin"><div heigth="40" weight="40" class="spinner-border" role="status"></div></div>`;
+    form.innerHTML += `<div class="spin"><div heigth="40" weight="40" class="spinner-border" role="status"></div></div>`;
     setTimeout(() => {
         document.querySelector(".spin").style.visibility = "hidden";
     }, 1500);
 }
 
 function login() {
-    let user = String(document.querySelectorAll(".login-input")[0].value);
-    let senha = String(document.querySelectorAll(".login-input")[1].value);
-    console.log(senha);
+    btnLogin.classList.remove("ativo");
+    const lblUsuario_valor = document.querySelector("#txtUsuario").value;
+    const lblSenha_valor = document.querySelector("#txtSenha").value;
 
     spin();
 
-    if (senha === senha_cadastrada && user === user_cadastrado) {
+    if (
+        lblSenha_valor === senhaCadastrada &&
+        lblUsuario_valor === usuarioCadastrado
+    ) {
         setTimeout(() => {
-            location.href = "/clone-league-of-legends/loading/";
+            // location.href = "/clone-league-of-legends/loading/";
+            location.href = "/loading/";
         }, 1500);
     } else {
-        let span_user = document.querySelectorAll(".login-span")[0];
-        let span_senha = document.querySelectorAll(".login-span")[1];
-        let imput_user = document.querySelectorAll(".login-input")[0];
+        let lblUsuario = document.querySelector(".lblUsuario");
+        let lblSenha = document.querySelector(".lblSenha");
 
-        span_user.style.color = "#c813d8";
-        imput_user.style.border = "2px solid #E7C0E8";
-        imput_user.style.backgroundColor = "#F3E2F4";
+        lblUsuario.style.color = "#c813d8";
+        txtUsuario.style.border = "2px solid #E7C0E8";
+        txtUsuario.style.backgroundColor = "#F3E2F4";
 
-        let imput_senha = document.querySelectorAll(".login-input")[1];
+        lblSenha.style.color = "#c813d8";
+        txtSenha.style.border = "2px solid #E7C0E8";
+        txtSenha.style.backgroundColor = "#F3E2F4";
 
-        span_senha.style.color = "#c813d8";
-        imput_senha.style.border = "2px solid #E7C0E8";
-        imput_senha.style.backgroundColor = "#F3E2F4";
-
-        document.querySelector(".login-form").innerHTML += `<span class="mensagem-erro">Suas credenciais de login não coincidem com uma conta em nosso sistema.</span>`;
+        form.innerHTML += `<span class="mensagem-erro">Suas credenciais de login não coincidem com uma conta em nosso sistema.</span>`;
     }
 }
 
-window.addEventListener("keydown", () => {
-    if (imput_user.value !== "" && imput_senha.value !== "") {
-        login_botao.style.backgroundColor = "#c73033";
-    }
-    if (imput_user.value == "" && imput_senha.value == "") {
-        login_botao.style.backgroundColor = "#fff";
+form.addEventListener("keyup", () => {
+    const lblUsuario_valor = document.querySelector("#txtUsuario").value;
+    const lblSenha_valor = document.querySelector("#txtSenha").value;
+
+    if (lblUsuario_valor === "" || lblSenha_valor === "") {
+        btnLogin.disabled = true;
+        btnLogin.classList.remove("ativo");
+    } else if (lblSenha_valor.length < 3 || lblUsuario_valor.length < 3) {
+        btnLogin.disabled = true;
+        btnLogin.classList.remove("ativo");
+    } else if (lblUsuario_valor !== "" && lblSenha_valor !== "") {
+        btnLogin.classList.add("ativo");
+        btnLogin.disabled = false;
     }
 });
-
-
