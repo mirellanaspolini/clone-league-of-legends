804b26b3646c48a0d14a4d8af3f0c48f355570d4
mi <mirellanaspolini35@gmail.com>
2023-08-17 10:35:59 -0300

    finalizado json de items em promoção

--- client/loja/destaques/script.js
+++ client/loja/destaques/script.js
@@ -1,12 +1,10 @@
-// alert("asdsa");
-
-async function carregarChamps() {
+async function carregarCards() {
     const dados = await fetch("./json/mais-populares.json");
     const json = await dados.json();
     const grid = document.querySelector("main");
-    // grid.innerHTML = "";
     json.forEach((item) => {
         console.log(item);
+
         grid.innerHTML += `<div class="card bumumb" data-percPromocao=${
             item.promotion[0] === true ? parseInt(item.promotion[1] * 100) : ""
         } data-promocao="true">
@@ -18,32 +16,22 @@ async function carregarChamps() {
          />
          <div class="card-info-bg">
              <p class="text-capitalize">${item.name}</p>
-             <span>
-             ${item.price[2]}
-                 <p class="preco-rp">${item.price[0]}</p>
-                 <p class="preco-ea">${item.price[1]}</p>
-                 <p>${item.price[2]}</p>
+             <span id="precos-box">
+                ${printaPreco(item)}
              </span>
          </div>
          </div>`;
     });
 }
 
-carregarChamps();
+const printaPreco = (item) => {
+    if (item.price[2]) {
+        return "<p>adquirido</p>";
+    }
+    if (item.price[1]) {
+        return `<p class="preco-rp">${item.price[0]}</p> <p class="preco-ea">${item.price[1]}</p>`;
+    }
+    return `<p class="preco-rp">${item.price[0]}</p>`
+};
 
-// `<div class="card bumumb" data-percPromocao="50" data-promocao="true">
-// <img
-//     src="https://live.staticflickr.com/65535/52265760794_0c16d57bc7_q.jpg"
-//     width="240"
-//     height="240"
-//     alt="Jax"
-// />
-// <div class="card-info-bg">
-//     <p class="text-capitalize">jax oriental</p>
-//     <span>
-//         <p class="preco-rp">375</p>
-//     </span>
-// </div>
-// </div>`;
-
-// grid.innerHTML +=
+carregarCards();
