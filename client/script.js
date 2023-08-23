a2eae18b4ceaff179874b78bc246b6c62375390e
mi <mirellanaspolini35@gmail.com>
2023-08-23 12:27:46 -0300

    refatorando

--- client/script.js
+++ client/script.js
@@ -1,7 +1,21 @@
-var header = document.querySelector(".header");
-var conteudo_geral = document.querySelector(".geral");
-var botao_inicio = document.querySelector(".botao-inicio");
-var botao_loja = document.querySelector(".botao-loja");
+const header = document.querySelector(".header");
+const listaClasses = header.classList;
+var conteudoGeral = document.querySelector(".geral");
+var botaoInicio = document.querySelector(".botao-inicio").classList;
+var botaoLoja = document.querySelector(".botao-loja").classList;
+
+const devs = [
+    {
+        nome: "Mirella Naspolini",
+        usuario: "https://github.com/mirellanaspolini",
+        linkedin: "https://www.linkedin.com/in/mirellanaspolini-12768322b/",
+    },
+    {
+        nome: "Vinícius Porto",
+        github: "https://github.com/ViniPorto",
+        linkedin: "https://www.linkedin.com/in/vinicius-porto-9a1996209/",
+    },
+];
 
 function abrir_inicio() {
     header.innerHTML = `
@@ -13,12 +27,11 @@ function abrir_inicio() {
             <li class="links-desabilitados">notas de atualização</li>
         </ul>
         </nav>`;
-    header.classList.add("header-inicio");
-    header.classList.remove("header-destaques");
-    header.classList.remove("header-campeoes");
+    listaClasses.add("header-inicio");
+    listaClasses.remove("header-destaques");
+    listaClasses.remove("header-campeoes");
 
-    var conteudo_geral = document.querySelector(".geral");
-    conteudo_geral.innerHTML = `<div id="carrossel" class="carousel slide" data-bs-ride="carousel">
+    conteudoGeral.innerHTML = `<div id="carrossel" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-indicators">
         <button type="button" data-bs-target="#carrossel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
         <button type="button" data-bs-target="#carrossel" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -34,129 +47,112 @@ function abrir_inicio() {
         <div class="carousel-item">
             <div class="desenvolvido">
                 <h2 style="margin-bottom: 18px;font-weight: 700;">DESENVOLVIDO POR:</h2>
-                <div class="info-devs">
-                    <div class="devs">
-                        <h3 style="margin-bottom: 6px;">Mirella Naspolini</h3>
-                        <a href="https://github.com/mirellanaspolini" target="_blank"><i style="margin-right: 6px;" class="fab fa fa-github" aria-hidden="true"></i></a>
-                        <a href="https://www.linkedin.com/in/mirellanaspolini-12768322b/" target="_blank"><i class="fab fa fa-linkedin-square" aria-hidden="true"></i></a>
-                    </div>
-                    
-                    <div class="devs">
-                        <h3 style="margin-bottom: 6px;">Vinícius Porto</h3>
-                        <a href="https://github.com/ViniPorto" target="_blank"><i style="margin-right: 6px;" class="fab fa fa-github" aria-hidden="true"></i></a>
-                        <a href="https://www.linkedin.com/in/vinicius-porto-9a1996209/" target="_blank"><i width="40" class="fab fa fa-linkedin-square" aria-hidden="true"></i></a>
-                    </div>
-                </div>
+                <div class="info-devs"> ${devs.forEach((dev) => {
+                    return `<div class="devs">
+                        <h3 style="margin-bottom: 6px;">${dev.nome}</h3>
+                        <a href="${dev.github}" target="_blank">
+                            <i style="margin-right: 6px;" class="fab fa fa-github" aria-hidden="true"></i>
+                        </a>
+                        <a href="${dev.linkedin}" target="_blank">
+                            <i class="fab fa fa-linkedin-square" aria-hidden="true"></i>
+                        </a>
+                    </div>`;
+                })} </div>
             </div>
         </div>
     </div>
     </div>
     <img src="https://live.staticflickr.com/65535/52280173860_fbdca2d4ee_h.jpg" width="800" height="510" alt="wallpaper">`;
 
-    botao_inicio.classList.add("ativo");
-    botao_loja.classList.remove("ativo");
+    botaoInicio.add("ativo");
+    botaoLoja.remove("ativo");
 }
 
 function abrir_loja() {
-    header.innerHTML = `
-    <nav>
-        <ul class="d-flex gap-4 text-uppercase">
-            <li style="cursor: pointer;">destaques</li>
-            <li style="cursor: pointer;" onclick="abrir_loja_campeoes()">campeões</li>
-            <li class="links-desabilitados">skins</li>
-            <li class="links-desabilitados">tft</li>
-            <li class="links-desabilitados">espólios</li>
-            <li class="links-desabilitados">acessórios</li>
-        </ul>
-    </nav>
-    <div class="d-flex gap-3 header-destaques-botoes">
-        <button class="links-desabilitados">COMPRE RP</button>
-        <button class="links-desabilitados"><img src="./img/loja/presentes-icone.svg" alt=""></button>
-        <button class="links-desabilitados"><img src="./img/loja/configuracoes-icone.svg" alt=""></button>
-    </div>`;
-    header.classList.add("header-destaques");
-    header.classList.remove("header-inicio");
-    header.classList.remove("header-campeoes");
+    textoNav("abrir_loja_campeoes()");
+    listaClasses.add("header-destaques");
+    listaClasses.remove("header-inicio");
+    listaClasses.remove("header-campeoes");
 
-    conteudo_geral.innerHTML = `<object style="height: 100%; width: 100%;" type="text/html" data="./loja/destaques/index.html"></object>`;
+    exibeConteudo("destaques");
 
-    botao_inicio.classList.remove("ativo");
-    botao_loja.classList.add("ativo");
+    botaoInicio.remove("ativo");
+    botaoLoja.add("ativo");
 }
 
 function abrir_loja_campeoes() {
-    let header = document.querySelector(".header");
-    header.innerHTML = `
-    <nav>
-        <ul class="d-flex gap-4 text-uppercase">
-            <li style="cursor: pointer;" onclick="abrir_loja()">destaques</li>
-            <li style="cursor: pointer;">campeões</li>
-            <li class="links-desabilitados">skins</li>
-            <li class="links-desabilitados">tft</li>
-            <li class="links-desabilitados">espólios</li>
-            <li class="links-desabilitados">acessórios</li>
-        </ul>
-    </nav>
-    <div class="d-flex gap-3 header-destaques-botoes">
-        <button class="links-desabilitados">COMPRE RP</button>
-        <button class="links-desabilitados"><img src="./img/loja/presentes-icone.svg" alt=""></button>
-        <button class="links-desabilitados"><img src="./img/loja/configuracoes-icone.svg" alt=""></button>
-    </div>`;
+    textoNav("abrir_loja()");
+
     header.classList.add("header-campeoes");
     header.classList.remove("header-destaques");
     header.classList.remove("header-inicio");
 
-    conteudo_geral.innerHTML = `<object style="height: 100%; width: 100%;" type="text/html" data="./loja/campeoes/index.html"></object>`;
+    exibeConteudo("campeoes");
 }
 
 async function carregarSocial() {
     const dados = await fetch("./json/social.json");
     const json = await dados.json();
     const wrapper = document.querySelector(".wrapper-pastas");
-    json.forEach((pasta, indice) => {
-        for (const [key, value] of Object.entries(pasta)) {
-            // console.log(value);
-            let totalamigos = 0;
-            if (value.status !== "Offline") {
-                totalamigos++;
-            }
 
+    const printaAmigos = (lista, pasta) => {
+        Object.entries(lista).forEach(([key, value]) => {
+            wrapper.querySelector(`#${pasta} ul`).innerHTML += `                
+            <li class="social-box">
+            <img width="36" class="social-img" src=${value.img} alt="">
+            <div>
+            <p style="color: #929994;">${value.name}</p>
+        <p class="status ${statusCor(value.status)}">${value.status}</p>
+                    </div>
+                </li>`;
+        });
+    };
+
+    json.forEach((pasta) => {
+        console.log(pasta);
+        for (const [key, value] of Object.entries(pasta)) {
             const nomepasta = key.replace(/ /g, "-");
 
-
             wrapper.innerHTML += `
-                 <button class="btn text-uppercase" type="button" data-bs-toggle="collapse" data-bs-target="#${nomepasta}">${key} (${totalamigos}/${
-                value.length
-            })</button>
+                 <button class="btn text-uppercase" type="button" data-bs-toggle="collapse" data-bs-target="#${nomepasta}">${key} (${value.length}/${value.length})</button>
             <div class="collapse multi-collapse" id="${nomepasta}">
                     <ul></ul>
                 </div>`;
 
-            Object.entries(value).forEach(([key, value]) => {
-                wrapper.querySelector(`#${nomepasta} ul`).innerHTML += `                
-                            <li class="social-box">
-                                <img width="36" class="social-img" src=${
-                                    value.img
-                                } alt="">
-                                <div>
-                                    <p style="color: #929994;">${value.name}</p>
-                                    <p class="status ${
-                                        value.status === "Em partida" || value.status === "Seleção de Campeões"
-                                            ? "azul"
-                                            : value.status === "Online"
-                                            ? "online"
-                                            : value.status === "Lol+"
-                                            ? "lol-plus"
-                                            : value.status === "Criando partida de TFT"
-                                            ? "ausente"
-                                            : "offline"
-                                    }">${value.status}</p>
-                                </div>
-                            </li>
-                `;
-            });
+            printaAmigos(value, nomepasta);
         }
     });
 }
+
+const textoNav = (funcaoDoOnclick) => {
+    header.innerHTML = `<nav>
+        <ul class="d-flex gap-4 text-uppercase">
+           <li style="cursor: pointer;" onclick="${funcaoDoOnclick}">destaques</li>
+        <li style="cursor: pointer;" onclick="${funcaoDoOnclick}">campeões</li>
+            <li class="links-desabilitados">skins</li>
+            <li class="links-desabilitados">tft</li>
+            <li class="links-desabilitados">espólios</li>
+            <li class="links-desabilitados">acessórios</li>
+        </ul>
+    </nav>
+    <div class="d-flex gap-3 header-destaques-botoes">
+        <button class="links-desabilitados">COMPRE RP</button>
+        <button class="links-desabilitados"><img src="./img/loja/presentes-icone.svg" alt=""></button>
+        <button class="links-desabilitados"><img src="./img/loja/configuracoes-icone.svg" alt=""></button>
+    </div>`;
+};
+
+const statusCor = (value) => {
+    if (value === "Em partida" || value === "Seleção de Campeões")
+        return "azul";
+    else if (value === "Online") return "online";
+    else if (value === "Lol+") return "lol-plus";
+    else if (value === "Criando partida de TFT") return "ausente";
+    else return "offline";
+};
+
+const exibeConteudo = (pagina) => {
+    conteudoGeral.innerHTML = `<object style="height: 100%; width: 100%;" type="text/html" data="./loja/${pagina}/index.html"></object>`;
+};
 
 carregarSocial();
